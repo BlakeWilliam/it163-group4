bb17979f3a0b4a7be18092f4754cdcab4fc21614
marstonlm <marston.luann@gmail.com>
2021-05-17 11:11:59 -0700

    Create 1-in-10 popup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-$(document).ready(() => {
-    // This code only runs after the DOM is loaded.
-    
-  });
+//@ts-check
   
-  
-  
+// 1 in 10 Chance JS Modal popup
+
+ function checkWinner() {
+   let randomInt = Math.floor(Math.random() * (10-0) +1);
+   console.log(randomInt);
+
+  if (randomInt === 8) {
+    return alert("You are a WINNER");
+  }
+
+ else {
+    return console.log("Not a winner. RandomInt is" + randomInt + ".");
+  }
+}
+
+checkWinner();
