b6b36c9fb875ad462db1407fecb850ff378d6043
marstonlm <marston.luann@gmail.com>
2021-05-22 08:35:15 -0700

    Created about.php based on staff.php

---
+++ about.php
@@ -0,0 +1,72 @@
+<?php include "includes/header.php";?>
+
+      <!-- START ABOUT -->
+      <div id="about" class="row justify-content-center">
+        <div class="col">
+          <h2>Cupcake Ipsum</h2>
+          <p>Ice cream lemon drops cookie pastry apple pie chocolate bar. Cake sweet dragée. Icing sugar plum cheesecake pastry jujubes sweet cake tootsie roll tart. Cupcake cupcake cake topping jelly. Dessert macaroon powder liquorice icing chupa chups pudding. Gingerbread sweet jujubes dragée muffin pastry.</p>
+        </div>
+      </div>
+      <!-- END CONTACT -->
+      <!-- START TEAM -->
+        <div id="team" class="row justify-content-center">
+            <div class="col ">            
+                <h1>Our Team</h1>
+            </div>
+        </div>
+
+      <div class="row justify-content-center">
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio A</h2>
+            <img src="images/staff_01.jpg" class="img-fluid" alt="">
+            <p>Donut pudding candy sugar plum. Liquorice candy halvah. Jujubes sesame snaps macaroon bonbon ice cream bear claw sesame snaps sweet cupcake. Gummi bears ice cream fruitcake lemon drops.</p>
+            <p>Contact <a href="url">Staff A</a></p>
+        </div>
+
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio B</h2>
+            <img src="images/staff_02.jpg" class="img-fluid" alt="">
+            <p>Oat cake cupcake wafer. Chocolate bar soufflé chocolate bar topping donut. Chupa chups candy macaroon soufflé chocolate cake sweet cake.</p>
+            <p>Contact <a href="url">Staff B</a></p>
+        </div>
+
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio C</h2>
+            <img src="images/staff_03.jpg" class="img-fluid" alt="">
+            <p>Brownie sweet roll brownie. Carrot cake cookie jujubes dragée fruitcake gummi bears. Chocolate bar fruitcake chocolate cake pastry marshmallow lollipop wafer fruitcake gummies. Chocolate wafer macaroon.</p>
+            <p>Contact <a href="url">Staff C</a></p>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio D</h2>
+            <img src="images/staff_01.jpg" class="img-fluid" alt="">
+            <p>Sweet roll chocolate bar wafer topping marzipan. Brownie cotton candy tart chocolate bar lemon drops cookie. Cookie cheesecake cheesecake tart gummi bears bear claw croissant. Pudding gummi bears macaroon candy canes dessert pastry sesame snaps sweet carrot cake.</p>
+            <p>Contact <a href="url">Staff D</a></p>
+        </div>
+
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio E</h2>
+            <img src="images/staff_02.jpg" class="img-fluid" alt="">
+            <p>Toffee dragée cupcake jujubes pie sweet roll pudding. Pastry danish pudding jelly gingerbread croissant croissant tart chocolate. Pudding sesame snaps fruitcake. Wafer caramels cheesecake cheesecake croissant chocolate lemon drops.</p>
+            <p>Contact <a href="url">Staff E</a></p>
+        </div>
+
+        <div class="col-sm-4 justify-content-center">
+            <h2>Staff Bio F</h2>
+            <img src="images/staff_03.jpg" class="img-fluid" alt="">
+            <p>Oat cake tiramisu oat cake. Chocolate cake chocolate jujubes donut sesame snaps tootsie roll jelly beans. Cupcake jelly beans chupa chups pudding sesame snaps dessert cotton candy candy canes. Dessert liquorice soufflé candy canes ice cream bear claw.</p>
+            <p>Contact <a href="url">Staff F</a></p>
+        </div>
+      </div>
+      <!-- END STAFF -->
+
+    </div>
+    <!-- END CONTAINER -->
+
+  </main>
+  <!-- END MAIN -->
+  <!-- START FOOTER-->
+  <?php include "indluces/footer.php";?>
+  
