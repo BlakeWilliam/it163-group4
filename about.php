6e88b95190c7d088fe81cdd99d08a35c0ba27c77
marstonlm <marston.luann@gmail.com>
2021-05-24 09:49:58 -0700

    Updated about.php w/ staff

--- about.php
+++ about.php
@@ -2,11 +2,14 @@
 
       <!-- START ABOUT -->
       <div id="about" class="row justify-content-center">
-        <div class="col">
-          <h2>Cupcake Ipsum</h2>
-          <p>Ice cream lemon drops cookie pastry apple pie chocolate bar. Cake sweet dragée. Icing sugar plum cheesecake pastry jujubes sweet cake tootsie roll tart. Cupcake cupcake cake topping jelly. Dessert macaroon powder liquorice icing chupa chups pudding. Gingerbread sweet jujubes dragée muffin pastry.</p>
+        <div class="col justify-content-left">
+          <h1>About Us</h1>
+          <p>Emerald City Urban Hiking Club is a project created and powered by volunteers who want to share their love of Seattle and the outdoors.</p>
+          <p>Our vision is to provide an opertunity for all to enjoy the outdoors regardless of ability or mode of transpertation. There's so much to see and do without leaving the city.</p>
+          <p>How do you Urban?</p>
         </div>
       </div>
+      <hr>
       <!-- END CONTACT -->
       <!-- START TEAM -->
         <div id="team" class="row justify-content-center">
@@ -17,50 +20,67 @@
 
       <div class="row justify-content-center">
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio A</h2>
-            <img src="images/staff_01.jpg" class="img-fluid" alt="">
-            <p>Donut pudding candy sugar plum. Liquorice candy halvah. Jujubes sesame snaps macaroon bonbon ice cream bear claw sesame snaps sweet cupcake. Gummi bears ice cream fruitcake lemon drops.</p>
-            <p>Contact <a href="url">Staff A</a></p>
+            <h2>Paskal Waller</h2>
+            <img src="images/staff_01.jpg" class="img-fluid" alt="Paskal Waller">
+            <p>Leads/orgnizes Retires on the Move hikes</p>
+            <p>Retirement doesn't alsways mean afternoon naps. Paskal is active in his Greenlake community and enjoys sharing his love of Seattle and the outdoors with people of all ages.</p>
+            <p>Contact Paskal<a href="url">Paskal</a></p>
         </div>
 
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio B</h2>
-            <img src="images/staff_02.jpg" class="img-fluid" alt="">
+            <h2>Alice Bellerose</h2>
+            <img src="images/staff_alice.jpg" class="img-fluid" alt="Alice Bellrose">
+            <p>Leads and Orgnizes: Hike it Baby and Family Groups</p>
             <p>Oat cake cupcake wafer. Chocolate bar soufflé chocolate bar topping donut. Chupa chups candy macaroon soufflé chocolate cake sweet cake.</p>
-            <p>Contact <a href="url">Staff B</a></p>
+            <p>Contact <a href="url">Alice</a></p>
         </div>
 
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio C</h2>
-            <img src="images/staff_03.jpg" class="img-fluid" alt="">
+            <h2>Elli Oliver</h2>
+            <img src="images/staff_elli.jpg" class="img-fluid" alt="Elli Oliver">
+            <p>Orginzes LGBT & Singles</p>
             <p>Brownie sweet roll brownie. Carrot cake cookie jujubes dragée fruitcake gummi bears. Chocolate bar fruitcake chocolate cake pastry marshmallow lollipop wafer fruitcake gummies. Chocolate wafer macaroon.</p>
-            <p>Contact <a href="url">Staff C</a></p>
+            <p>Contact <a href="url">Elli</a></p>
         </div>
       </div>
 
       <div class="row justify-content-center">
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio D</h2>
-            <img src="images/staff_01.jpg" class="img-fluid" alt="">
+            <h2>Blake Agrade</h2>
+            <img src="images/staff_01.jpg" class="img-fluid" alt="Blake Agrade">
+            <p>Co-Founder <br />Site Web Designer/Developer <br /> Orginzer/Lead ..... & .....</p>
             <p>Sweet roll chocolate bar wafer topping marzipan. Brownie cotton candy tart chocolate bar lemon drops cookie. Cookie cheesecake cheesecake tart gummi bears bear claw croissant. Pudding gummi bears macaroon candy canes dessert pastry sesame snaps sweet carrot cake.</p>
-            <p>Contact <a href="url">Staff D</a></p>
+            <p>Contact <a href="url">Blake</a></p>
         </div>
 
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio E</h2>
-            <img src="images/staff_02.jpg" class="img-fluid" alt="">
+            <h2>Luann Marston</h2>
+            <img src="images/staff_02.jpg" class="img-fluid" alt="Luann Marston">
+            <p>Co-Founder <br /> Orginzer of Women's Hike</p>
             <p>Toffee dragée cupcake jujubes pie sweet roll pudding. Pastry danish pudding jelly gingerbread croissant croissant tart chocolate. Pudding sesame snaps fruitcake. Wafer caramels cheesecake cheesecake croissant chocolate lemon drops.</p>
-            <p>Contact <a href="url">Staff E</a></p>
+            <p>Contact <a href="url">Luann</a></p>
         </div>
 
         <div class="col-sm-4 justify-content-center">
-            <h2>Staff Bio F</h2>
-            <img src="images/staff_03.jpg" class="img-fluid" alt="">
-            <p>Oat cake tiramisu oat cake. Chocolate cake chocolate jujubes donut sesame snaps tootsie roll jelly beans. Cupcake jelly beans chupa chups pudding sesame snaps dessert cotton candy candy canes. Dessert liquorice soufflé candy canes ice cream bear claw.</p>
-            <p>Contact <a href="url">Staff F</a></p>
+            <h2>Abby Marston</h2>
+            <img src="images/staff_abby.JPG" class="img-fluid" alt="Abby the pup">
+            <p>Pack Leader of Pup Hikes</p>
+            <p>In my spare time I enjoy louging on the couch and watching Scooby-Do. Don't let my love of couches fool you, I'm always up for a hike rain, snow but especially sun. My person, Luann Marston, is the one who actually plans Pup Hikes but I'm the insperation. Plus I'm the one leading the pack!</p>
+            <p>Contact <a href="url">Abby Marston</a></p>
         </div>
       </div>
+      <hr>
       <!-- END STAFF -->
+      <div id="about" class="row justify-content-center">
+        <div class="col justify-content-left">
+          <h1>Volunteer</h1>
+          <p>Our club only exists because of our volunteers and their love of urban hiking. Interested in volunteer? Not sure you have skills?</p>
+          <p>We are always looking for volunteers to help with orginazing and leading hikes. However we need voluntters elsewhere too. Do you like social media? Help us keep our Instagram up-to-date. Do you like web design? Help us keep our site up-to-date. <br /> Still not sure? Please contact us. All are welcome.
+          </p>
+        </div>
+      </div>
+      <!-- START VOLUNTEER -->
+      
 
     </div>
     <!-- END CONTAINER -->
@@ -68,5 +88,5 @@
   </main>
   <!-- END MAIN -->
   <!-- START FOOTER-->
-  <?php include "indluces/footer.php";?>
+  <?php include "includes/footer.php";?>
   
