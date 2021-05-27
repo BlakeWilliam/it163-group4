e600af187818213f2fe5df2f23f6ab507d4d76c5
marstonlm <marston.luann@gmail.com>
2021-05-27 14:44:59 -0700

    about.phps text content

--- about.php
+++ about.php
@@ -1,85 +1,88 @@
 <?php include "includes/header.php";?>
 
       <!-- START ABOUT -->
-      <div id="about" class="row justify-content-center">
-        <div class="col justify-content-left">
+      <div id="about" class="row">
+        <div class="col">
           <h1>About Us</h1>
-          <p>Emerald City Urban Hiking Club is a project created and powered by volunteers who want to share their love of Seattle and the outdoors.</p>
-          <p>Our vision is to provide an opertunity for all to enjoy the outdoors regardless of ability or mode of transpertation. There's so much to see and do without leaving the city.</p>
-          <p>How do you Urban?</p>
+          <p class="justify-text lead">Emerald City UHC is created and powered by volunteers want to share their love of Seattle and the outdoors.</p>
+          <p class="justify-text">Our vision is a world where everyone can connect with the outdoors no matter where they live, economics, beliefs, abilities and so much more. We strive to create a safe and welcoming community that brings people together who want to enjoy Urban hiking. It’s our volunteers who make this possible.</p>
+          
         </div>
       </div>
       <hr>
       <!-- END CONTACT -->
       <!-- START TEAM -->
-        <div id="team" class="row justify-content-center">
+        <div id="team" class="row">
             <div class="col ">            
                 <h1>Our Team</h1>
             </div>
         </div>
 
-      <div class="row justify-content-center">
-        <div class="col-sm-4 justify-content-center">
-            <h2>Paskal Waller</h2>
-            <img src="images/staff_01.jpg" class="img-fluid" alt="Paskal Waller">
-            <p>Leads/orgnizes Retires on the Move hikes</p>
-            <p>Retirement doesn't alsways mean afternoon naps. Paskal is active in his Greenlake community and enjoys sharing his love of Seattle and the outdoors with people of all ages.</p>
-            <p>Contact Paskal<a href="url">Paskal</a></p>
+      <div class="row">
+        <div class="col-sm-4">
+            <h2>Paskal <br />Waller</h2>
+            <img src="images/staff_paskal.jpg" class="Paksal Waller" alt="Paskal Waller">
+            <p class="justify-text"><em>Adventure Guide: On the Go</em></p>
+            <p class="justify-text">Retirement doesn't alsways mean afternoon naps. Paskal is active in his Greenlake community and enjoys sharing his love of Seattle and the outdoors with people of all ages.</p>
+            <p>Contact <a href="url">Paskal</a></p>
         </div>
 
-        <div class="col-sm-4 justify-content-center">
-            <h2>Alice Bellerose</h2>
+        <div class="col-sm-4">
+            <h2>Alice <br />Bellerose</h2>
             <img src="images/staff_alice.jpg" class="img-fluid" alt="Alice Bellrose">
-            <p>Leads and Orgnizes: Hike it Baby and Family Groups</p>
-            <p>Oat cake cupcake wafer. Chocolate bar soufflé chocolate bar topping donut. Chupa chups candy macaroon soufflé chocolate cake sweet cake.</p>
+            <p class="justify-text"><em>Adventure Guide: Family Hikers</em></p>
+            <p class="justify-text">Oat cake cupcake wafer. Chocolate bar soufflé chocolate bar topping donut. Chupa chups candy macaroon soufflé chocolate cake sweet cake.</p>
             <p>Contact <a href="url">Alice</a></p>
         </div>
 
-        <div class="col-sm-4 justify-content-center">
-            <h2>Elli Oliver</h2>
-            <img src="images/staff_elli.jpg" class="img-fluid" alt="Elli Oliver">
-            <p>Orginzes LGBT & Singles</p>
-            <p>Brownie sweet roll brownie. Carrot cake cookie jujubes dragée fruitcake gummi bears. Chocolate bar fruitcake chocolate cake pastry marshmallow lollipop wafer fruitcake gummies. Chocolate wafer macaroon.</p>
+        <div class="col-sm-4">
+            <h2>Elli <br />Oliver</h2>
+            <img src="images/staff_elli.png" class="img-fluid" alt="Elli Oliver">
+            <p class="justify-text"><em>Adventure Guide: LGBTQ</em></p>
+            <p class="justify-text">Brownie sweet roll brownie. Carrot cake cookie jujubes dragée fruitcake gummi bears. Chocolate bar fruitcake chocolate cake pastry marshmallow lollipop wafer fruitcake gummies. Chocolate wafer macaroon.</p>
             <p>Contact <a href="url">Elli</a></p>
         </div>
       </div>
 
-      <div class="row justify-content-center">
-        <div class="col-sm-4 justify-content-center">
-            <h2>Blake Agrade</h2>
+      <div class="row">
+        <div class="col-sm-4">
+            <h2>Blake <br />Agrade</h2>
             <img src="images/staff_01.jpg" class="img-fluid" alt="Blake Agrade">
-            <p>Co-Founder <br />Site Web Designer/Developer <br /> Orginzer/Lead ..... & .....</p>
-            <p>Sweet roll chocolate bar wafer topping marzipan. Brownie cotton candy tart chocolate bar lemon drops cookie. Cookie cheesecake cheesecake tart gummi bears bear claw croissant. Pudding gummi bears macaroon candy canes dessert pastry sesame snaps sweet carrot cake.</p>
+            <p class="justify-text"><em>Adventure Guide: 21 Plusers
+            <br />Co-Founder</em></p>
+            <p class="justify-text">Sweet roll chocolate bar wafer topping marzipan. Brownie cotton candy tart chocolate bar lemon drops cookie. Cookie cheesecake cheesecake tart gummi bears bear claw croissant. Pudding gummi bears macaroon candy canes dessert pastry sesame snaps sweet carrot cake.</p>
             <p>Contact <a href="url">Blake</a></p>
         </div>
 
-        <div class="col-sm-4 justify-content-center">
-            <h2>Luann Marston</h2>
+        <div class="col-sm-4">
+            <h2>Luann <br />Marston</h2>
             <img src="images/staff_02.jpg" class="img-fluid" alt="Luann Marston">
-            <p>Co-Founder <br /> Orginzer of Women's Hike</p>
-            <p>Toffee dragée cupcake jujubes pie sweet roll pudding. Pastry danish pudding jelly gingerbread croissant croissant tart chocolate. Pudding sesame snaps fruitcake. Wafer caramels cheesecake cheesecake croissant chocolate lemon drops.</p>
+            <p class="justify-text"><em>Adventure Guide: Puppers
+            <br>Co-Founder</em></p>
+            <p class="justify-text">Toffee dragée cupcake jujubes pie sweet roll pudding. Pastry danish pudding jelly gingerbread croissant croissant tart chocolate. Pudding sesame snaps fruitcake. Wafer caramels cheesecake cheesecake croissant chocolate lemon drops.</p>
             <p>Contact <a href="url">Luann</a></p>
         </div>
 
-        <div class="col-sm-4 justify-content-center">
-            <h2>Abby Marston</h2>
+        <div class="col-sm-4">
+            <h2>Abby <br />Marston</h2>
             <img src="images/staff_abby.JPG" class="img-fluid" alt="Abby the pup">
-            <p>Pack Leader of Pup Hikes</p>
-            <p>In my spare time I enjoy louging on the couch and watching Scooby-Do. Don't let my love of couches fool you, I'm always up for a hike rain, snow but especially sun. My person, Luann Marston, is the one who actually plans Pup Hikes but I'm the insperation. Plus I'm the one leading the pack!</p>
-            <p>Contact <a href="url">Abby Marston</a></p>
+            <p class="justify-text"><em>Pack Guide: Puppers</em></p>
+            <p class="justify-text">In my spare time I enjoy louging on the couch and watching Scooby-Do. Don't let my love of couches fool you, I'm always up for a hike rain, snow but especially sun. My person, Luann Marston, is the one who actually plans Pup Hikes but I'm the insperation. Plus I'm the one leading the pack!</p>
+            <p>Contact <a href="url">Abby</a></p>
         </div>
-      </div>
+      </div> 
       <hr>
       <!-- END STAFF -->
-      <div id="about" class="row justify-content-center">
-        <div class="col justify-content-left">
+      <!-- START VOLUNTEER -->
+      <div id="volunteer" class="row">
+        <div class="col">
           <h1>Volunteer</h1>
-          <p>Our club only exists because of our volunteers and their love of urban hiking. Interested in volunteer? Not sure you have skills?</p>
-          <p>We are always looking for volunteers to help with orginazing and leading hikes. However we need voluntters elsewhere too. Do you like social media? Help us keep our Instagram up-to-date. Do you like web design? Help us keep our site up-to-date. <br /> Still not sure? Please contact us. All are welcome.
-          </p>
+          <p class="justify-text">Emerald City UHC’s adventure guides consists of approximately seven old and new core volunteers. Non-hierarchical committees meet regularly to organize and plan hikes, events, and activities.</p>
+          <p class="justify-text">Our culture of peer-to-peer learning supports new volunteers who are motivated to become adventure guides. In addition, about 15 regular volunteers help behind the scenes with our keeping social media, ensuring our website is up-to-date, reaching out to the community. They represent many age groups, ethnicities, and physical abilities.</p> 
+          <p class="justify-text">Want to learn more, please contact us.</p>
         </div>
       </div>
-      <!-- START VOLUNTEER -->
+      <!-- END VOLUNTEER -->
       
 
     </div>
