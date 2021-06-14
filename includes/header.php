44be44736c614c6fa647d0ef08dd92704d554fd3
marstonlm <marston.luann@gmail.com>
2021-06-14 10:16:22 -0700

    Update header.php meta tags

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,9 @@
 
 <head>
   <!-- Required meta tags -->
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="index, follow">
 
   <!-- Bootstrap Layout CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -20,6 +21,7 @@
   <link rel="stylesheet" href="https://use.typekit.net/omb7zgd.css">
 
   <title>Emerald City UHC</title>
+  <meta name="description" content="Emerald City UHC:Urban Hiking Club in Seattle Washington.">
 </head>
 
 <body onload="openForm()">
