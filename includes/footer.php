333eb139af07c2c127b59fa7041fa63a4ac889e5
marstonlm <marston.luann@gmail.com>
2021-06-14 13:29:43 -0700

    Update footer.php w/ current alljavascript.js file

--- includes/footer.php
+++ includes/footer.php
@@ -61,11 +61,8 @@
 
 <!-- Optional JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<!-- commented out while testing js file 
 <script src="js/alljavascript.js"></script>
--->
 
-<script src="js/test.js"></script>
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
