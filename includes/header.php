02c76745b02df5eb0542065f47b4791a6d72d43e
marstonlm <marston.luann@gmail.com>
2021-06-14 10:18:46 -0700

    Update meta tag

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="robots" content="index, follow">
+  <meta name="google-site-verification" content="8ITOve7oNkpGDE83TFWvaHKObxSajQEiABcAbCXGmGw" />
 
   <!-- Bootstrap Layout CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
