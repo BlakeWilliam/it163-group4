6bbc3f4006c6675c8cc59758492bc88d6ee6cb5c
marstonlm <marston.luann@gmail.com>
2021-06-14 17:35:47 -0700

    Updated javascript.js Removed most console.log

--- js/alljavascript.js
+++ js/alljavascript.js
@@ -22,18 +22,14 @@ setInterval(function () {
 
 // preventOnload keeps the survey and winner popups on the index page.
 function preventOnload() {
-  if (window.location.href === 'http://www.luannmarston.com/it163-group4/index.php') {
-  
+  if (window.location.href === 'http://www.luannmarston.com/it163-group4/index.php') {  
   //!!!!!!!NEED TO CHANGE HREF IF ON DIFFERENT SERVER!!!!!!!
-    console.log('preventOnoad says we are on the index.php page.');
-    console.log("page location is" + window.location.href);
+    console.log("page location is " + window.location.href + ". Is that the location you want?");
 
     //@ts-check
-
     // 1 in 10 Chance JS Modal popup
     function checkWinner() {
       let randomInt = Math.floor(Math.random() * (10 - 0) + 1);
-      console.log(randomInt);
 
       if (randomInt === 8) {
         let a = document.createElement('a');
@@ -44,14 +40,9 @@ function preventOnload() {
           a.click();
         };
       }
-
-      else {
-        return console.log("Not a winner. RandomInt is" + randomInt + ".");
-      }
     }
 
     checkWinner();
-
 
     // START SURVEY
     function openForm() {
@@ -85,72 +76,51 @@ function preventOnload() {
       switch (selectedValuePic) {
         case 'cat':
           document.getElementById('animalchange').src = "images/survey_cat.jpg";
-          console.log('image of cat');
           break;
 
         case 'dog':
           document.getElementById('animalchange').src = 'https://earthnworld.com/wp-content/uploads/2018/05/Dog-2-scaled.jpg';
-          console.log('image of dog');
           break;
 
         case 'dolphin':
           document.getElementById('animalchange').src = 'https://earthnworld.com/wp-content/uploads/2017/12/DOLPHIN-1024x731.jpg';
-          console.log('image of dolphin');
           break;
 
         case 'tiger':
           document.getElementById('animalchange').src = 'https://earthnworld.com/wp-content/uploads/2017/04/Tiger-1024x684.jpg';
-          console.log('image of tigar');
           break;
 
         case 'fox':
           document.getElementById('animalchange').src = 'https://earthnworld.com/wp-content/uploads/2018/12/Sleeping-Fox-1024x640.jpg';
-          console.log('image of fox');
           break;
 
         case 'squirrel':
           document.getElementById('animalchange').src = 'https://www.seacoastonline.com/storyimage/SO/20180916/NEWS/180919513/AR/0/AR-180919513.jpg';
-          console.log('image of squirrel');
           break;
 
         default:
           document.getElementById('animalchange').src = 'images/mainseattle.png';
-          console.log('animal pic did not work');
       }
 
       /* HTML and console output */
       /* ----- CHANGE NAME ----- */
       if (!inputName.value) {
         outputName.innerHTML = "Hiker";
-        console.log('User did not suggest name');
       }
       else {
         outputName.innerHTML = inputName.value;
-        console.log("inputName.value is " + inputName.value);
       }
 
       /* ----- CHANGE FONT SIZE ----- */
       if (inputAge.value > 59) {
         document.documentElement.style.fontSize = "1.5em";
-        console.log('user over 59 and font should be larger');
-      }
-      else {
-        console.log('user is younger than 60 or did not answer');
-      }
-      /* ----- END CHANGE FONT SIZE ----- */
 
-
+      }
       // Close POP form w/ submitBtn
       document.getElementById("popupForm").style.display = "none";
     }
 
-
     //END SURVEY
-  }
-
-  else {
-    console.log('preventOnoad says we are not on the index.php page.');
-    console.log("page location is" + window.location.href);
   }
 }
 // Close Popup form w/ close btn
